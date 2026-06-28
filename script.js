a7a30dba6afde45520e4d8410a8408e63e8dccaf
Ewan <ewanwd2008@gmail.com>
2026-06-28 16:17:34 +0100

    enemy hit sprite, exp moves and bobs and floats towards player.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("game"); //canvas element form html
 const ctx = canvas.getContext("2d"); //drawing tools 
+ctx.imageSmoothingEnabled = false;
 
 const inventoryMenu = document.getElementById("inventoryMenu")
 
@@ -10,6 +11,7 @@ const heartImage = new Image()
 const playerRight = new Image()
 const playerMoveLeft = new Image()
 const playerMoveRight = new Image()
+const enemyHit = new Image()
 
 const enemyImages = [
     new Image(),
@@ -23,6 +25,9 @@ const weaponImages = [
     new Image(),
 ]
 
+const expImage = new Image()
+const grassImage = new Image()
+
 playerLeft.src = "images/left.png"
 playerMoveLeft.src = "images/left-walk.png"
 playerRight.src = "images/right.png"
@@ -31,19 +36,27 @@ playerMoveRight.src = "images/right-walk.png"
 enemyImages[0].src = "images/green-slime.png"
 enemyImages[1].src = "images/teal-slime.png"
 enemyImages[2].src = "images/blue_slime.png"
+enemyHit.src = "images/hit_enemy.png"
 
 weaponImages[0].src = "images/pistol.png"
 weaponImages[1].src = "images/smg.png"
 weaponImages[2].src = "images/shotgun.png"
 
 heartImage.src = "images/heart.png"
+expImage.src = "images/exp2.png"
+grassImage.src = "images/grass.png"
+
 //canvas width and height variables 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+window.addEventListener("resize", resize);
+resize();
 
 let gameOver = false;
 let inventoryOpen = false;
-
+let damageFlash = 0;
 let score = 0;
 
 let walkFrame = false;
@@ -77,7 +90,7 @@ const player = {
 
 const weapons = [
     {name: "pistol", damage: 10, cooldown: 500, bulletSpeed: 8,  bulletSize: 8, projectiles: 1, owned: true,   icon: weaponImages[0]},
-    {name: "SMG",    damage: 2,  cooldown: 100, bulletSpeed: 10, bulletSize: 6, projectiles: 1, owned: false,  icon: weaponImages[1]},
+    {name: "SMG",    damage: 3,  cooldown: 100, bulletSpeed: 10, bulletSize: 6, projectiles: 1, owned: false,  icon: weaponImages[1]},
     {name: "shotgun",damage: 8,  cooldown: 800, bulletSpeed: 7,  bulletSize: 7, projectiles: 5, owned: false,  icon: weaponImages[2]},
 ]
 
@@ -95,6 +108,10 @@ inventoryButton.addEventListener("click", () => {
 let equippedWeapon =weapons[0];
 let lastShot = 0;
 
+const camera = {
+    x: player.x - canvas.width/2,
+    y: player.y - canvas.height/2
+}
 
 //checks when a key is pressed 
 document.addEventListener("keydown", (e) => {
@@ -106,6 +123,8 @@ document.addEventListener("keyup", (e) => {
     keys[e.key.toLowerCase()] = false; //changes the key release = false
 })
 
+
+
 function spawnEnemy () { //creates a single new enemie
     const side = Math.floor(Math.random() * 4); //chosing a random side of the screen
     let x;
@@ -113,26 +132,26 @@ function spawnEnemy () { //creates a single new enemie
 
     //topp edge spawn
     if (side ==0) {
-        x=Math.random() * canvas.width;
-        y = -30;
+        x=camera.x + Math.random() * canvas.width;
+        y = camera.y - 50;
     }
 
     //rdght edge spawn
     if (side == 1) {
-        x= canvas.width + 30;
-        y =Math.random() * canvas.height;
+        x= camera.x + canvas.width + 30;
+        y = camera.y+Math.random() * canvas.height;
     }
 
     //bottom edge spawn
     if (side == 2) {
-        x=Math.random() * canvas.width;
-        y = canvas.height + 30
+        x= camera.x+Math.random() * canvas.width;
+        y = camera.y + canvas.height + 30
     }
 
     //left edge spawn
     if (side == 3) {
-        x = -30;
-        y = Math.random() * canvas.height;
+        x = camera.x-30;
+        y = camera.y+Math.random() * canvas.height;
     }
 
     enemies.push({
@@ -141,9 +160,11 @@ function spawnEnemy () { //creates a single new enemie
         size: 100,
         hitbox: 20,
         speed:2,
+        flashTime:0,
         lastHit: 0,
         hp: 20,
-        sprite: enemyImages[Math.floor(Math.random()*3)]
+        sprite: enemyImages[Math.floor(Math.random()*3)],
+        hitsprite: enemyHit
     })
 }
 
@@ -195,32 +216,48 @@ function shootNearestEnemy() {
 
 //updates game logic every frame
 function update() {
+    if (inventoryOpen) return;
+    camera.x = player.x - canvas.width / 2;
+    camera.y = player.y - canvas.height / 2;
     player.moving = false;
-    if (keys["w"]){
-        player.y -= player.speed;
+    let moveX = 0;
+    let moveY = 0;
+
+    if (keys["w"]) {
+        moveY--;
         player.moving = true;
     }
 
     if (keys["s"]) {
-        player.y += player.speed;
+        moveY++;
         player.moving = true;
     }
 
     if (keys["a"]) {
-        player.x -= player.speed;
+        moveX--;
         player.direction = "left";
-        player.moving = true;
     }
 
     if (keys["d"]) {
-        player.x += player.speed;
-            player.direction = "right";
+        moveX++;
+        player.direction = "right";
+    }
+    const length = Math.hypot(moveX, moveY);
+    if (length > 0) {
+        moveX /= length;
+        moveY /= length;
+
+        player.x += moveX * player.speed;
+        player.y += moveY * player.speed;
         player.moving = true;
     }
 
 
     //loop through every enemy
     for (const enemy of enemies) {
+        if (enemy.flashTime > 0) {
+            enemy.flashTime--;
+        }
         const dx = player.x - enemy.x; // adj 
         const dy = player.y - enemy.y; // op
 
@@ -237,6 +274,7 @@ function update() {
 
             if (now - enemy.lastHit > 1000) {
                 player.hp -= 1;
+                damageFlash = 10;
                 enemy.lastHit = now;
             }
         }
@@ -270,13 +308,20 @@ function update() {
 
             if (distance < hitDistance) {
                 enemy.hp -= bullet.damage;
+                enemy.flashTime = 6;
                 bullets.splice(b,1);
+                const orbSize = Math.floor(Math.random() * 31) + 20;
                 if (enemy.hp <= 0) {
                     gems.push({
                         x:enemy.x,
                         y: enemy.y,
-                        value: 1,
-                        size: 10,
+                        size: orbSize,
+                        value: (Math.floor(orbSize / 10)),
+                        bobOffset: Math.random() * Math.PI * 2,
+                        rotation: 0,
+                        pulse: Math.random() * Math.PI * 2,
+                        vx:0,
+                        vy:0
                     });
                     enemies.splice(e,1);
                     score++;
@@ -293,9 +338,23 @@ function update() {
         const dx = player.x - gem.x;
         const dy = player.y - gem.y;
         const distance = Math.hypot(dx,dy);
-        const collectionDistance = player.size / 2 + gem.size / 2
 
-        if (distance < collectionDistance) {
+        gem.bobOffset += 0.08;
+        gem.rotation += 0.05;
+        gem.pulse += 0.1;
+
+        if (distance < 300) {
+            const pullStrength = 0.15;
+            gem.vx += (dx/distance) * pullStrength;
+            gem.vy += (dy/distance) * pullStrength;
+        }
+
+        gem.x += gem.vx
+        gem.y += gem.vy
+        gem.vx *= 0.9
+        gem.vy *= 0.9
+        const collisionDistance = player.size / 2 + gem.size / 2;
+        if (distance < collisionDistance) {
             player.xp += gem.value;
             gems.splice(i,1)
         }
@@ -307,12 +366,33 @@ function update() {
         player.xpNeeded = Math.floor(player.xpNeeded * 1.5)
         
     }
-
+    if (damageFlash > 0) {
+        damageFlash--;
+    }
 }
 
 //draws everything everyframe
 function draw() {
+
+    let shakeX = 0;
+    let shakeY = 0;
+    if (damageFlash > 0) {
+        shakeX = (Math.random() - 0.5) * 8;
+        shakeY = (Math.random() - 0.5) * 8;
+    }
+    ctx.save();
+    ctx.translate(shakeX,shakeY);
+
     ctx.clearRect(0,0, canvas.width, canvas.height); //clear previous 
+
+    const TILE_SIZE = 96
+    const startX = Math.floor(camera.x / TILE_SIZE) * TILE_SIZE;
+    const startY = Math.floor(camera.y /TILE_SIZE) * TILE_SIZE;
+    for (let x=startX; x < camera.x + canvas.width; x += TILE_SIZE) {
+        for (let y=startY; y <camera.y + canvas.height; y += TILE_SIZE) {
+            ctx.drawImage(grassImage,x -camera.x,y-camera.y,TILE_SIZE,TILE_SIZE)
+        }
+    }
 
     let aimAngle = 0;
 
@@ -360,11 +440,11 @@ function draw() {
         }
     }
 
-    ctx.drawImage(currentPlayerImage,player.x-64,player.y-64,96,96);
+    ctx.drawImage(currentPlayerImage,canvas.width/2-64,canvas.height/2-64,96,96);
 
     ctx.save();
     ctx.translate(
-        player.x, player.y
+        canvas.width/2, canvas.height/2
     );
     ctx.rotate(aimAngle)
     ctx.drawImage(
@@ -374,18 +454,21 @@ function draw() {
     ctx.restore();
 
     for (const enemy of enemies) {
+        const image = enemy.flashTime > 0
+            ? enemy.hitsprite
+            : enemy.sprite;
         ctx.drawImage( //drawing the enemies 
-            enemy.sprite,
-            enemy.x - enemy.size /2,
-            enemy.y - enemy.size /2,
+            image,
+            enemy.x - enemy.size /2 -camera.x,
+            enemy.y - enemy.size /2-camera.y,
             enemy.size,
             enemy.size
         )
 
         ctx.fillStyle = "green"
         ctx.fillRect(
-            enemy.x - 15,
-            enemy.y - 20,
+            enemy.x - 15 - camera.x,
+            enemy.y - 20 - camera.y,
             (enemy.hp/20) * 30,4
         )
     }
@@ -404,44 +487,60 @@ function draw() {
         ctx.fillStyle = "yellow"
 
         ctx.fillRect(
-            bullet.x - bullet.size / 2,
-            bullet.y - bullet.size / 2,
+            bullet.x - bullet.size / 2 - camera.x,
+            bullet.y - bullet.size / 2 - camera.y,
             bullet.size,
             bullet.size
         )
     }
 
     for (const gem of gems) {
-        ctx.fillStyle = "lime";
-        ctx.fillRect(
-            gem.x - gem.size/2,
-            gem.y - gem.size/2,
-            gem.size,
-            gem.size
-        )
+        const floatY = Math.sin(gem.bobOffset) *8
+        const scale = 1 + Math.sin(gem.pulse) * 0.1
+        ctx.save();
+        ctx.translate(gem.x - camera.x, gem.y-camera.y + floatY);
+        ctx.rotate(gem.rotation);
+        ctx.drawImage(
+            expImage,
+            -gem.size/2*scale,
+            -gem.size/2*scale,
+            gem.size*scale,
+            gem.size*scale
+        );
+        ctx.restore();
     }
 
     ctx.fillStyle = "white";
-    ctx.font = "30px Arial";
+    ctx.font = "30px 'font'";
     ctx.fillText("score: " + score, 20, 80)
 
-    ctx.fillStyle = "grey";
-    ctx.fillRect(20, canvas.height - 30, 300, 20);
+    const x = 230;
+    const y = 25;
+    const width = 300;
+    const height = 20;
 
-    ctx.fillStyle = "lime";
-    ctx.fillRect(20, canvas.height - 30, (player.xp/player.xpNeeded) * 300, 20);
+    ctx.fillStyle = "#1b0d31";
+    ctx.fillRect(x,y,width,height);
+    ctx.fillStyle = "#8c3cff";
+    ctx.fillRect(x,y,(player.xp / player.xpNeeded) * width, height)
+    ctx.lineWidth = 3;
+    ctx.strokeStyle ="white";
+    ctx.strokeRect(x,y,width,height);
+    ctx.font ="20px 'font'";
+    ctx.fillText(`Level ${player.level}`,350,85)
 
-    ctx.fillStyle ="white";
-    ctx.font ="20px Arial";
-    ctx.fillText(`Level ${player.level}`,20,canvas.height - 40)
-
+    ctx.restore();
+    if (damageFlash > 0) {
+        ctx.fillStyle = `rgba(255,0,0,${damageFlash/20})`;
+        ctx.fillRect(0,0,canvas.width, canvas.height);
+    }
 }
 
 //game loop
 function gameloop() {
     if (gameOver) {
         ctx.fillStyle = "white";
-        ctx.font = "60px Arial";
+        ctx.font = "60px 'font'";
         ctx.fillText("Game Over", canvas.width / 2 - 170, canvas.height / 2);
         return;
     }
